0a52b4e33384a83a6be2a5b7d20e142082a17d23
Alex <alexsandro.ardian45@gmail.com>
2023-12-18 20:43:13 +0700

    Update Commit Js

--- JS/Template/scrolltopbtn.js
+++ JS/Template/scrolltopbtn.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         $('html, body').animate({
             scrollTop: 0
         }, {
-            duration: 50,
+            duration: 30,
             easing: 'easeInQuad'
         });
     });

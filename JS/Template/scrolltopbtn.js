54c2f8e95c2942a3ecbd2059144832996ce4c97f
Alex <alexsandro.ardian45@gmail.com>
2023-12-18 20:39:47 +0700

    innit commit

---
+++ JS/Template/scrolltopbtn.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('#topBtn').fadeIn();
+        } else {
+            $('#topBtn').fadeOut();
+        }
+    });
+
+    $("#topBtn").click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, {
+            duration: 50,
+            easing: 'easeInQuad'
+        });
+    });
+});

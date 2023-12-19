ff0ddb3e5f751cb4a16627996c354b589d804930
Alex <alexsandro.ardian45@gmail.com>
2023-12-19 16:22:58 +0700

    innit commit

--- JS/Template/scrolltopbtn.js
+++ JS/Template/scrolltopbtn.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 500) {
+        if ($(this).scrollTop() > 300) {
             $('#topBtn').fadeIn();
         } else {
             $('#topBtn').fadeOut();
